c966b6b40b9170216851060b0470f282cdce2c0b
aashra01 <aashrapandey00@gmail.com>
2025-12-26 19:35:03 +0545

    upload document feature

---
+++ app/(auth)/upload-document/page.tsx
@@ -0,0 +1,10 @@
+// app/upload-document/page.tsx
+'use client';
+
+import UploadDocument from "../_components/UploadDocument";
+
+
+
+export default function UploadDocumentPage() {
+  return <UploadDocument />;
+}
