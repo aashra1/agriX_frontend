911e6a4c7e8766be413fb6eea01fefcf0bf11eb1
aashra01 <aashrapandey00@gmail.com>
2026-02-11 20:42:37 +0545

    Update API endpoint definitions

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -4,6 +4,8 @@ export const API = {
     LOGIN: "/api/user/login",
     GET_ALL: "/api/user",
     GET_PROFILE: (id: string) => `/api/user/${id}`,
+    REQUEST_PASSWORD_RESET: "/api/user/request-password-reset",
+    RESET_PASSWORD: (token: string) => `/api/user/reset-password/${token}`,
   },
   BUSINESS: {
     REGISTER: "/api/business/register",
