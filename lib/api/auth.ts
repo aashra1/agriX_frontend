4672d2a075719e6b7e9799fcbaf0bfdf2fccd26f
aashra01 <aashrapandey00@gmail.com>
2026-02-11 20:42:33 +0545

    Improve auth API integration

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -22,3 +22,31 @@ export const loginUser = async (loginData: any) => {
     throw err;
   }
 };
+
+export const requestPasswordReset = async (email: string) => {
+  try {
+    const response = await axiosInstance.post(API.USER.REQUEST_PASSWORD_RESET, {
+      email,
+    });
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message ||
+        error.message ||
+        "Request password reset failed",
+    );
+  }
+};
+
+export const resetPassword = async (token: string, newPassword: string) => {
+  try {
+    const response = await axiosInstance.post(API.USER.RESET_PASSWORD(token), {
+      newPassword: newPassword,
+    });
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Reset password failed",
+    );
+  }
+};
