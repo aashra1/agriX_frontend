9993b5e9e07aeeafe243fbec0ed1115030b29558
aashra01 <aashrapandey00@gmail.com>
2026-02-15 13:28:29 +0545

    feat: implement axios interceptor for automatic auth headers

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,14 +1,25 @@
+// lib/api/axios.ts
 import axios from "axios";
+import { getAuthToken } from "@/lib/cookie";
 
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5001";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
-export default axiosInstance;
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = await getAuthToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
+export default axiosInstance;
