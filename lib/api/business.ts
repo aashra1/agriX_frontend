c0fd9202b026f41206abce3a91e533fd2a3ce46f
aashra01 <aashrapandey00@gmail.com>
2026-02-17 15:50:53 +0545

    Add business order API methods

--- lib/api/business.ts
+++ lib/api/business.ts
@@ -1,11 +1,16 @@
 import axiosInstance from "./axios";
 import { API } from "./endpoints";
 
-export const registerBusiness = async (registerData: any) => {
+export const registerBusiness = async (registerData: FormData) => {
   try {
     const response = await axiosInstance.post(
       API.BUSINESS.REGISTER,
       registerData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
     );
     return response.data;
   } catch (err: any) {
@@ -27,9 +32,58 @@ export const uploadBusinessDocument = async (formData: FormData) => {
     const response = await axiosInstance.post(
       API.BUSINESS.UPLOAD_DOC,
       formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
     );
     return response.data;
   } catch (err: any) {
     throw err;
+  }
+};
+
+export const getBusinessProfile = async () => {
+  try {
+    const response = await axiosInstance.get(API.BUSINESS.GET_PROFILE);
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const updateBusinessProfile = async (formData: FormData) => {
+  try {
+    const response = await axiosInstance.put(
+      API.BUSINESS.UPDATE_PROFILE,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
+    );
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const getAllBusinesses = async () => {
+  try {
+    const response = await axiosInstance.get(API.BUSINESS.GET_ALL);
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const approveBusiness = async (businessId: string) => {
+  try {
+    const response = await axiosInstance.put(API.BUSINESS.APPROVE(businessId));
+    return response.data;
+  } catch (error: any) {
+    throw error;
   }
 };
