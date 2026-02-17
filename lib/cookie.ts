d59f81a8878c9c5d3d55e29ec76ef11173627bf3
aashra01 <aashrapandey00@gmail.com>
2026-02-17 15:50:54 +0545

    Update cookie utilities

--- lib/cookie.ts
+++ lib/cookie.ts
@@ -3,7 +3,6 @@ import { cookies } from "next/headers";
 
 export const setAuthToken = async (token: string) => {
   const cookieStore = await cookies();
-  // cookieStore.set({ name: "auth_token", value: token })
   cookieStore.set({
     name: "auth_token",
     value: token,
@@ -13,28 +12,61 @@ export const setAuthToken = async (token: string) => {
     secure: process.env.NODE_ENV === "production",
   });
 };
+
 export const getAuthToken = async () => {
   const cookieStore = await cookies();
   const token = cookieStore.get("auth_token")?.value;
   return token || null;
 };
+
+export const setTempToken = async (token: string) => {
+  const cookieStore = await cookies();
+  cookieStore.set({
+    name: "temp_token",
+    value: token,
+    httpOnly: true,
+    sameSite: "lax",
+    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 3600, 
+  });
+};
+
+export const getTempToken = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("temp_token")?.value;
+  return token || null;
+};
+
+export const clearTempToken = async () => {
+  const cookieStore = await cookies();
+  cookieStore.delete("temp_token");
+};
+
 export const setUserData = async (userData: any) => {
   const cookieStore = await cookies();
-  // cookie can only store string
-  // convert object to string -> JSON.stringify "{}"
-  cookieStore.set({ name: "user_data", value: JSON.stringify(userData) });
+  cookieStore.set({
+    name: "user_data",
+    value: JSON.stringify(userData),
+    httpOnly: true,
+    sameSite: "lax",
+    path: "/",
+    secure: process.env.NODE_ENV === "production",
+  });
 };
+
 export const getUserData = async () => {
   const cookieStore = await cookies();
   const userData = cookieStore.get("user_data")?.value;
   if (userData) {
-    // convert string to object -> JSON.parse
     return JSON.parse(userData);
   }
   return null;
 };
+
 export const clearAuthCookies = async () => {
   const cookieStore = await cookies();
   cookieStore.delete("auth_token");
   cookieStore.delete("user_data");
+  cookieStore.delete("temp_token");
 };
